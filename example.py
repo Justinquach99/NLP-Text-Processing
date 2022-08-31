5be7c9ba40d36367a279e8c0d33f8da3da0a7f0b
Justinquach99 <109769268+Justinquach99@users.noreply.github.com>
2022-08-30 21:58:51 -0700

    Test

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ def tokens_lowercase(doc):
     tok = metapy.analyzers.Porter2Filter(tok)
     ana = metapy.analyzers.NGramWordAnalyzer(3, tok)
     trigrams = ana.analyze(doc)
-    
+     
     #leave the rest of the code as is
     tok.set_content(doc.content())
     tokens, counts = [], []
